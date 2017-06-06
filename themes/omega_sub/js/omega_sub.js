548d10b2fab9036cb4e88f95d20c080edbadc330
maxavj <maxav.job@gmail.com>
2017-06-06 14:59:03 +0300

    fix our-story

--- themes/omega_sub/js/omega_sub.js
+++ themes/omega_sub/js/omega_sub.js
@@ -50,9 +50,11 @@ var $item = $('.carousel .item');
      $("#loadMore").on('click', function (e) {
          e.preventDefault();
          $("p:hidden").slice(0, 1).slideDown();
+
          if ($("p:hidden").length == 0) {
              $("#load").fadeOut('slow');
          }
+
          $('html,body').animate({
              scrollTop: $(this).offset().top
          }, 1500);
